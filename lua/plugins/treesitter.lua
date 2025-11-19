d6c9a1de7f823b23c9f055b397ced1ac24976cb9
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-19 20:37:16 +0100

    enable treesitter highlights

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     "nvim-treesitter",
     event = "DeferredUIEnter",
     opts = {
+      highlight = {
+        enable = true,
+      },
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
