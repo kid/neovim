773805c442d5d8e3a07aa7b09c8585681ac41869
Arnaud_Rebts <arnaud.rebts@gmail.com>
2025-11-20 12:09:02 +0100

    fix: disable codecompanion's blink integration

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,21 +1,13 @@
+--- @type lz.n.PluginSpec[]
 return {
   {
     "codecompanion.nvim",
     dependencies = {
       "plenary.nvim",
       "nvim-treesitter",
-      "blink.cmp",
     },
     after = function()
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            opts = {
-              completion_provider = "blink",
-            },
-          },
-        },
-      })
+      require("codecompanion").setup({})
     end,
   },
 }
