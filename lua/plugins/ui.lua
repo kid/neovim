f47157941bde8f6ce07fdca083881c44de4b9bc1
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-22 09:20:55 +0100

    chore: more cleanup

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -27,36 +27,18 @@ return {
         options = {
           global_status = true,
         },
-        -- sections = {
-        --   lualine_x = {
-        --     {
-        --       require("noice").api.status.message.get_hl,
-        --       cond = require("noice").api.status.message.has,
-        --     },
-        --     {
-        --       require("noice").api.status.command.get,
-        --       cond = require("noice").api.status.command.has,
-        --       color = { fg = "#ff9e64" },
-        --     },
-        --     {
-        --       require("noice").api.status.mode.get,
-        --       cond = require("noice").api.status.mode.has,
-        --       color = { fg = "#ff9e64" },
-        --     },
-        --     {
-        --       require("noice").api.status.search.get,
-        --       cond = require("noice").api.status.search.has,
-        --       color = { fg = "#ff9e64" },
-        --     },
-        --   },
-        -- },
       })
     end,
   },
   {
     "which-key.nvim",
     event = "DeferredUIEnter",
-    dependencies = { "mini.icons" },
+    before = function()
+      LZE.trigger_load("mini.icons")
+    end,
+    after = function()
+      require("which-key").setup({})
+    end,
   },
   {
     "noice.nvim",
