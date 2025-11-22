b4dbb85bb95232c8651b7e7aad3d12e9f8987919
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-22 12:53:22 +0100

    feat: add some basic keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,18 @@
 
 -- Disable Space bar since it will be used as the leader key
 vim.keymap.set({ "n", "v" }, "<leader>", "<nop>")
+
+-- stylua: ignore start
+-- System clipboard yanks/pastes
+vim.keymap.set({ "n", "v" }, "<leader>y",  '"+y',  { desc = "Yank to system clipboard" })
+vim.keymap.set({ "n" },      "<leader>Y",  '"+y$', { desc = "Yank line to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p",  '"+p',  { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>P",  '"+P',  { desc = "Paste before from system clipboard" })
+
+-- Save with Ctrl+S
+vim.keymap.set({ "n", "v" }, "<C-s>", "<Esc><cmd>w<cr>",  { desc = "Save file" })
+vim.keymap.set({ "i", },     "<C-s>", "<Esc><cmd>w<cr>i", { desc = "Save file" })
+
+-- Disable command-line window
+vim.keymap.set("n", "q:", "<nop>")
+-- stylua: ignore end
