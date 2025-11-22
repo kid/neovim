51bc8a3e65dcebb7b4413dc93f5f00b10c89ca7e
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-22 13:01:48 +0100

    feat: add a keymap to close buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,8 +15,8 @@ vim.keymap.set({ "n", "v" }, "<leader>", "<nop>")
 -- System clipboard yanks/pastes
 vim.keymap.set({ "n", "v" }, "<leader>y",  '"+y',  { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n" },      "<leader>Y",  '"+y$', { desc = "Yank line to system clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>p",  '"+p',  { desc = "Paste from system clipboard" })
-vim.keymap.set({ "n", "v" }, "<leader>P",  '"+P',  { desc = "Paste before from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p",  '"+p',  { desc = "Paste from system clipboard (after)" })
+vim.keymap.set({ "n", "v" }, "<leader>P",  '"+P',  { desc = "Paste from system clipboard (before)" })
 
 -- Save with Ctrl+S
 vim.keymap.set({ "n", "v" }, "<C-s>", "<Esc><cmd>w<cr>",  { desc = "Save file" })
@@ -24,4 +24,12 @@ vim.keymap.set({ "i", },     "<C-s>", "<Esc><cmd>w<cr>i", { desc = "Save file" }
 
 -- Disable command-line window
 vim.keymap.set("n", "q:", "<nop>")
+
+vim.keymap.set("n", "<leader>q", function()
+  if vim.bo.modified then
+    vim.notify("Buffer modified. Use :w or :bd! to force.", vim.log.levels.WARN)
+    return
+  end
+  vim.cmd("bd")
+end, { desc = "Close buffer" })
 -- stylua: ignore end
