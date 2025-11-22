20023194c7fa4d7beddd0cb3e5915d8f13af5367
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-22 09:16:55 +0100

    chore: code cleanup

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -7,7 +7,6 @@ return {
   {
     "lspkind.nvim",
     event = "DeferredUIEnter",
-    opts = {},
   },
   { "nvim-web-devicons" },
   {
@@ -49,38 +48,6 @@ return {
                   end,
                 },
               },
-              --   kind_icon = {
-              --     text = function(ctx)
-              --       local icon = ctx.kind_icon
-              --       if vim.tbl_contains({ "Path" }, ctx.source_name) then
-              --         local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-              --         if dev_icon then
-              --           icon = dev_icon
-              --         end
-              --       else
-              --         icon = require("lspkind").symbolic(ctx.kind, {
-              --           mode = "symbol",
-              --         })
-              --       end
-              --
-              --       return icon .. ctx.icon_gap
-              --     end,
-              --
-              --     -- Optionally, use the highlight groups from nvim-web-devicons
-              --     -- You can also add the same function for `kind.highlight` if you want to
-              --     -- keep the highlight groups in sync with the icons.
-              --     highlight = function(ctx)
-              --       local hl = ctx.kind_hl
-              --       if vim.tbl_contains({ "Path" }, ctx.source_name) then
-              --         local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-              --         if dev_icon then
-              --           hl = dev_hl
-              --         end
-              --       end
-              --       return hl
-              --     end,
-              --   },
-              -- },
             },
           },
           documentation = {
@@ -100,16 +67,15 @@ return {
         signature = { enabled = true },
         sources = {
           default = {
-            "lazydev",
             "lsp",
             "path",
             -- "snippets",
             "buffer",
             "copilot",
           },
-          -- per_filetype = {
-          --   codecompanion = { "codecompanion" },
-          -- },
+          per_filetype = {
+            lua = { "lazydev", inherit_defaults = true },
+          },
           providers = {
             lazydev = {
               name = "LazyDev",
