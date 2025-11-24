6dd77b8904931e895264a7ef50bdb54ab500575f
Arnaud_Rebts <arnaud.rebts@gmail.com>
2025-11-24 13:21:33 +0100

    feat: add flash.nvim

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -105,4 +105,21 @@ return {
       require("blink.pairs").setup({})
     end,
   },
+  -- TODO: move this somewhere else
+  {
+    "flash.nvim",
+    event = "DeferredUIEnter",
+    after = function()
+      require("flash").setup({})
+    end,
+    keys = {
+      -- stylua: ignore start
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump()              end, desc = "Flash"             },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter()        end, desc = "Flash Treesitter"  },
+      { "r", mode = "o",               function() require("flash").remote()            end, desc = "Remote Flash"      },
+      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      -- stylua: ignore end
+    },
+  },
 }
