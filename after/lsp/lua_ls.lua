3cc06118943521ca9da46c052d95a60d78a428d7
Arnaud_Rebts <arnaud.rebts@gmail.com>
2025-11-24 11:48:46 +0100

    fix: don't set workspace library for lua_ls in after/lsp/lua_ls.lua
    It's already taken care of by lazydev.nvim

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,10 +1,8 @@
+--- @type vim.lsp.Config
 return {
   settings = {
     Lua = {
       hint = { enable = true },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
     },
   },
 }
