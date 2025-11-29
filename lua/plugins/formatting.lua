d8623eb9d620038dbb4c7341363b23b0484e1fc1
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-11-29 11:55:18 +0100

    feat: add formatting for shell scripts

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -16,6 +16,7 @@ return {
         formatters_by_ft = {
           lua = { "treefmt", "stylua", stop_after_first = true },
           nix = { "treefmt", "nixfmt", stop_after_first = true },
+          sh = { "treefmt", "shfmt", stop_after_first = true },
           go = { "goimports-reviser", "gofmt" },
           ts = { "prettierd", "prettier", "eslint", stop_after_first = true },
           js = { "prettierd", "prettier", "eslint", stop_after_first = true },
