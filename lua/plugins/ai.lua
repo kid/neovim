f1dbe03678bef6bc3b7d0ede7eaf6364837faa25
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-12-06 17:25:42 +0100

    fix: disable inline diff for NES

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -41,6 +41,9 @@ return {
       require("sidekick").setup({
         nes = {
           enabled = true,
+          diff = {
+            inline = false,
+          },
         },
         signs = {
           enabled = true,
