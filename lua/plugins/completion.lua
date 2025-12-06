fe81a62b200026d92aae97fbce62dab05c7dd3a9
Arnaud Rebts <arnaud.rebts@gmail.com>
2025-12-06 11:54:21 +0100

    fix(flash): separate forward and backward jumps

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -120,11 +120,8 @@ return {
     end,
     keys = {
       -- stylua: ignore start
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump()              end, desc = "Flash"             },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter()        end, desc = "Flash Treesitter"  },
-      { "r", mode = "o",               function() require("flash").remote()            end, desc = "Remote Flash"      },
-      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { forward = true,  wrap = false, multi_window = false } }) end, desc = "Flash"  },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { forward = false, wrap = false, multi_window = false } }) end, desc = "Flash"  },
       -- stylua: ignore end
     },
   },
