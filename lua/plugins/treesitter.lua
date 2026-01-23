fbf116b64c57f1d8be3f71970bd5dadbb7ea6d62
Arnaud Rebts <arnaud.rebts@gmail.com>
2026-01-23 11:43:56 +0100

    fix: auto enable treesitter highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,18 +3,38 @@
 return {
   {
     "nvim-treesitter",
-    event = "DeferredUIEnter",
-    load = function(name)
-      require("lzextras").loaders.multi({
-        name,
-        "nvim-treesitter-textobjects",
+    lazy = false,
+    -- load = function(name)
+    --   require("lzextras").loaders.multi({
+    --     name,
+    --     "nvim-treesitter-textobjects",
+    --   })
+    -- end,
+    after = function()
+      local nvim_treesitter = require("nvim-treesitter")
+      nvim_treesitter.setup()
+      local available = nvim_treesitter.get_available()
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "*" },
+        callback = function(args)
+          local bufnr = args.buf
+          local ft = args.match
+
+          if vim.list_contains(available, ft) then
+            vim.treesitter.start(bufnr)
+          end
+        end,
       })
     end,
+  },
+  {
+    "nvim-treesitter-textobjects",
+    lazy = false,
     before = function()
-      -- vim.g.no_plugin_maps = true
+      LZE.trigger_load("nvim-treesitter")
     end,
     after = function()
-      require("nvim-treesitter").setup()
       require("nvim-treesitter-textobjects").setup()
     end,
   },
